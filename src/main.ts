8300efebfb60dd1d5d27f45e09bb18fbcc1cc0e2
leandro3306 <leandro.vieira@pag.ai>
2022-05-22 16:31:14 -0300

    chore: config docker and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Football league manager API')
+    .setDescription(
+      'A manager to help soccer teams to create a fun and competitive environment.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
